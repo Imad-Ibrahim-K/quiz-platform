b02b3f59aeba4acb3aec74410c727ee3f78fcea8
Imad Ibrahim <imadibrahim164@gmail.com>
2023-11-10 02:47:18 +0530

    added singup logic

--- claint/views.py
+++ claint/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from .models import UserProfile
 
 # Create your views here.
 def signin(request):
@@ -8,6 +12,28 @@ def signin(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        fullName = request.POST.get("fullName")
+        userName = request.POST.get("userName")
+        password = request.POST.get("pass")
+        password2 = request.POST.get("passCon")
+        emailId = request.POST.get("emailId")
+
+        # Validate the password
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            # The password is not valid, return the error messages
+            return render(request, "registration_page.html", {"errors": e.messages})
+
+        # Check if the passwords match
+        if password != password2:
+            return render(request, "registration_page.html", {"errors": ["Passwords do not match"]})
+
+        # Create the user
+        user = User.objects.create_user(userName, emailId, password)
+        profile = UserProfile(user=user, full_name=fullName)
+        profile.save()
 
     return render(request, "registration_page.html")
 
